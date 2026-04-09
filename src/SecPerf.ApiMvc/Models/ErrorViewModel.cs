0c572837f5dc9f18d4e5f8be354b44d302cc3fbc
Kacper Pasierb <kacperpasierb@protonmail.com>
2026-04-09 19:12:26 +0200

    - project structure

---
+++ src/SecPerf.ApiMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecPerf.ApiMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
